88eb982e8a08eb31676dfa096575532dd0929907
saxrick <artemsofin12@gmail.com>
2023-11-02 12:34:30 +0500

    mod7

---
+++ buns/mod7/src/main/java/com/artemsofin/spring/security/configuration/MyConfig.java
@@ -0,0 +1,42 @@
+package com.artemsofin.spring.security.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@ComponentScan(basePackages = "com.artemsofin.spring.security")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver =
+                new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource datasource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false&jdbcCompliantTruncation=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
+}
---
+++ buns/mod7/src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
+<!DOCTYPE html>
+<html>
+<body>
+<h3>Information for all employees</h3>
+<br><br>
+<security:authorize access="hasRole('HR')">
+<input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
+only for HR staff
+</security:authorize>
+<br><br>
+<security:authorize access="hasRole('MANAGER')">
+<input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
+only for Managers
+</security:authorize>
+</body>
+</html>
